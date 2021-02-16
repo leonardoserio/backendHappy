9c1e8d7085a3a7a302d0d4d782ac07b235f35e06
Leonardo <newmysms@gmail.com>
2021-02-16 11:35:46 -0300

    move code to routes

--- src/server.ts
+++ src/server.ts
@@ -1,38 +1,11 @@
 import express from 'express';
 import './database/connection';
-import { getRepository } from 'typeorm';
-import Orphanage from './models/Orphanage';
+import routes from './routes';
 
 const app = express();
+
 app.use(express.json());
 
-app.post('/orphanages', async (request, response) => {
-  console.log(request.body);
-  const {
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends,
-  } = request.body;
-
-  const orphanagesRepository = getRepository(Orphanage);
-
-  const orphanage = orphanagesRepository.create({
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends,
-  });
-
-  await orphanagesRepository.save(orphanage);
-
-  return response.status(201).json(orphanage);
-});
+app.use(routes);
 
 app.listen(3333);
